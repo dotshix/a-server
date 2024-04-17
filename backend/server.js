8aed50316debd0169c3d89467e1814c8bb2b2020
Dario <dariosans@gmail.com>
2024-04-17 15:51:35 -0400

    back to normal

--- backend/server.js
+++ backend/server.js
@@ -197,6 +197,4 @@ const port = process.env.PORT || 7000;
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
-  const result = await scrapeSearch("Pencil Case");
-  console.log(result);
 });
